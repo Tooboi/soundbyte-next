f4eb28d1e09c3e9b6e0d639e4b0870212465d488
Tooboi <japica123@gmail.com>
2023-08-02 13:17:57 -0400

    Clerk Auth Functional

---
+++ app/components/Navbar/Navbar.js
@@ -0,0 +1,38 @@
+import Link from 'next/link';
+import { UserButton, currentUser, auth } from '@clerk/nextjs';
+
+export default async function Navbar() {
+  const { userId } = auth();
+  const user = await currentUser();
+//   console.log(user);
+
+  return (
+    <>
+      <nav className="bg-stone-800 py-4 px-6 flex items-center justify-between mb-5">
+        <div className="flex items-center">
+          <Link href="/">
+            <div className="text-lg font-bold text-emerald-400">SoundByte</div>
+          </Link>
+        </div>
+        <div className="text-stone-200 flex items-center">
+          {!userId && (
+            <>
+              <Link href="sign-in" className="text-stone-200 hover:text-stone-300 mr-4">
+                Sign In
+              </Link>
+              <Link href="sign-up" className="text-stone-200 hover:text-stone-300 mr-4">
+                Sign Up
+              </Link>
+            </>
+          )}
+          { userId && (
+            <Link href='profile' className='text-stone-200 hover:text-stone-300 mr-4'>Profile</Link>
+          )}
+          <div className='ml-auto'>
+            <UserButton afterSignOutUrl='/' />
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
