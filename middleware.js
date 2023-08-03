d9d9ecad0fec74aa288e908673e852a5ae9f6c19
Tooboi <japica123@gmail.com>
2023-08-02 20:31:24 -0400

    middleware

--- middleware.js
+++ middleware.js
@@ -1,9 +1,10 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-    publicRoutes: ['/', '/register']
+  publicRoutes: ['/', '/register', '/dashboard']
 });
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
+
