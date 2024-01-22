720723436b721cf04bae1fafd41ad5ae53b87dc1
Tooboi <japica123@gmail.com>
2024-01-21 21:23:50 -0500

    Sign in page removed bloat

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,9 +1,4 @@
 import { authOptions } from "../../api/auth/[...nextauth]/route";
-import type {
-  GetServerSidePropsContext,
-  InferGetServerSidePropsType,
-} from "next";
-import { getProviders, signIn } from "next-auth/react";
 import { getServerSession } from "next-auth";
 
 import GoogleSignInWrapper from "@/components/GoogleSignInWrapper";
@@ -41,22 +36,4 @@ export default async function SignIn() {
       </div>
     </>
   );
-}
-
-export async function getServerSideProps(context: GetServerSidePropsContext) {
-  const session = await getServerSession(context.req, context.res, authOptions);
-
-  // If the user is already logged in, redirect.
-  // Note: Make sure not to redirect to the same page
-  // To avoid an infinite loop!
-  if (session) {
-    return { redirect: { destination: "/" } };
-  }
-
-  const providers = await getProviders();
-  console.log("Providers:" + providers);
-
-  return {
-    props: { providers },
-  };
 }
